e7069798ad9a48ecdf019405c2de07e752bcd11a
CHAVA <181G0232@rcarbonifera.tecnm.mx>
2022-01-19 18:09:11 -0600

    First Commit

---
+++ Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public partial class Tag
+    {
+        public int Id { get; set; }
+        public string Key { get; set; } = null!;
+        public string Value { get; set; } = null!;
+    }
+}
---
+++ Models/tagsContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApp.Models
+{
+    public partial class tagsContext : DbContext
+    {
+        public tagsContext()
+        {
+        }
+
+        public tagsContext(DbContextOptions<tagsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tag> Tags { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;user=root;password=root;database=tags", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.6.5-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("latin1_swedish_ci")
+                .HasCharSet("latin1");
+
+            modelBuilder.Entity<Tag>(entity =>
+            {
+                entity.ToTable("tag");
+
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.Key).HasColumnType("tinytext");
+
+                entity.Property(e => e.Value).HasColumnType("tinytext");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
